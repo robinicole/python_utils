80870d67df5a502c03c87e44e145fe1d66a80771
Robin Nicole <robin.nicole@sojern.com>
2023-02-24 14:06:35 +0000

    add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='python_utils',
+      version='1.0',
+      description='Python helpers',
+      author='Robin Nicole',
+      author_email='robin.nicole.m@gmail.com',
+      packages=['python_utils'],
+     )
