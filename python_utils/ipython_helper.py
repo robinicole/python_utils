740cd39628f754847c9236e6ce4f6a1b689b6048
Robin Nicole <robin.nicole@sojern.com>
2023-02-25 12:41:17 +0000

    add ipython and pandas helpers

---
+++ python_utils/ipython_helper.py
@@ -0,0 +1,6 @@
+def print_ipython_notebook_header():
+    print("""%load_ext autoreload
+%autoreload 2
+import os 
+import sys
+sys.path.append('/Users/robinnicole/workspace/code/python/kubeflow/')""")
